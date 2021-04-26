a55bc86e9ddb3391c18040b5c8ac4425aa7f420b
Ayomide Ibosiola <ayomideibosiola100@gmail.com>
2021-04-26 11:38:34 +0100

    next commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full overflow-y-auto h-full z-20 ">
+  <div class="w-full overflow-y-auto h-full z-20">
     <section class="z-30 font-medium">
       <section id="top" class="z-30 px-44 pt-6 flex justify-between">
         <div style="font-size: 10px" class="flex space-x-3 justify-between">
@@ -257,71 +257,64 @@
         Thank you for your interest in Dayspring Capital. Please fill out the form below
         to ask your questions. We will get back to you within the shortest possible time.
       </p>
-      <div class="flex justify-between w-full space-x-9">
-        <div class="inset-0 object-cover w-1/3">
-          <img
-            class="w-80"
-            style="height: 524px"
-            src="../assets/message.svg"
-            alt="send message"
-          />
-        </div>
-        <div class="w-2/3 space-y-10">
-          <div class="w-full flex">
-            <div class="w-1/2">
-              <label for="fname" class="" style="font-size: 15px">First Name</label>
-              <br />
-              <input type="text" class="rounded-xl" placeholder="e.g John" />
-            </div>
-
-            <div class="w-1/2">
-              <label for="sname" class="" style="font-size: 15px">Second Name</label>
-              <br />
-              <input
-                type="text"
-                placeholder="e.g Doe"
-                class="px-14 py-2 rounded-xl focus:outline-none focus:ring focus:border-blue-300 mt-1"
-              />
-            </div>
-          </div>
-
-          <div class="w-full flex">
-            <div class="w-1/2">
-              <label for="email" class="" style="font-size: 15px">Email Address</label>
-              <br />
-              <input
-                type="email"
-                placeholder="e.g johndoe@email.com"
-                class="px-14 py-2 rounded-xl focus:outline-none focus:ring focus:border-blue-300 mt-1"
-              />
-            </div>
-
-            <div class="w-1/2">
-              <label for="p/number" class="" style="font-size: 15px">Phone Number</label>
-              <br />
-              <input
-                type="number"
-                placeholder="e.g 08023456789"
-                class="px-14 py-2 rounded-xl focus:outline-none focus:ring focus:border-blue-300 mt-1"
-              />
-            </div>
-          </div>
-
-          <div class="w-full">
-            <label for="message" class="" style="font-size: 15px">Message</label>
-            <br />
-            <input
-              type="text"
-              placeholder="Write your message here"
-              class="pr-96 pl-14 pt-6 pb-24 rounded-xl focus:outline-none focus:ring focus:border-blue-300 mt-1"
+      <div class="flex justify-between w-full">
+        <div class="w-full flex space-x-12">
+          <div class="inset-0 object-cover w-1/3">
+            <img
+              class="w-80"
+              style="height: 524px"
+              src="../assets/message.svg"
+              alt="send message"
             />
           </div>
-          <button
-            style="background: #1da737"
-            class="text-white rounded-md py-4 px-7 text-xl font-semibold"
-          >
-            Read More
-          </button>
+          <div class="w-2/3 space-y-10">
+            <div class="w-full flex space-x-4">
+              <div class="w-1/2">
+                  <label for="fname" class="" style="font-size: 15px">First Name</label>
+                  <br />
+                  <input type="text" class="rounded-xl w-full" placeholder="e.g John" />
+                </div>
+
+              <div class="w-1/2">
+                  <label for="sname" class="" style="font-size: 15px">Second Name</label>
+                  <br />
+                  <input type="text" class="rounded-xl w-full" placeholder="e.g Doe" />
+                </div>
+            </div>
+
+            <div class="w-full flex space-x-4">
+              <div class="w-1/2">
+                  <label for="email" class="" style="font-size: 15px"
+                    >Email Address</label
+                  >
+                  <br />
+                  <input
+                    type="text"
+                    class="rounded-xl w-full"
+                    placeholder="e.g johndoe@email.com"
+                  />
+              </div>
+              <div class="w-1/2">
+                  <label for="fname" class="" style="font-size: 15px">Phone Number</label>
+                  <br />
+                  <input type="text" class="rounded-xl w-full" placeholder="e.g 08023456789" />
+                </div>
+            </div>
+
+            <div class="w-full">
+              <label for="message" class="" style="font-size: 15px">Message</label>
+              <br />
+              <textarea name="message" id="" cols="90" rows="10" class="rounded-xl">
+                Write your message here
+              </textarea>
+            </div>
+            <button
+              style="background: #1da737"
+              class="text-white rounded-md py-4 px-7 text-xl font-semibold"
+            >
+              Read More
+            </button>
+          </div>
         </div>
       </div>
     </section>
